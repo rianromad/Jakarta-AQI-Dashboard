06010e9d8b3a9e0e17426c75f007789794e3e140
Subkhan Rian Romadhon <61647791+rianromad@users.noreply.github.com>
2023-11-20 10:14:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 with open('style.css')as f:
  st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
-@st.cache_data(ttl="2h", persist="disk")
+@st.cache_data(ttl="2h", persist=False)
 def get_data():
 
     #get data
